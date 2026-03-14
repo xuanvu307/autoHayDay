a413747edf2756d8133ce248feccf62db1e8de88
xuanvu307 <xuanvufb2@gmail.com>
2026-03-15 00:46:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -300,11 +300,11 @@ function showDashboard() {
         showSettingPage(data);
     });
 
-    ui.cardLogout.on("click", function () {
-        storage.remove("user_key");
-        engines.execScriptFile(engines.myEngine().getSource());
-        engines.myEngine().forceStop();
-    });
+    // ui.cardLogout.on("click", function () {
+    //     storage.remove("user_key");
+    //     engines.execScriptFile(engines.myEngine().getSource());
+    //     engines.myEngine().forceStop();
+    // });
 }
 
 function uiUpbarn(data) {
